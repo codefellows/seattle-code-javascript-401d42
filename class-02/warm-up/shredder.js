36fa1f5b51a9bcdec3ad06c6973f32028662961d
JacobKnaack <jacobknck@gmail.com>
2021-06-15 13:02:51 -0700

    updates warm up file content

--- class-02/warm-up/shredder.js
+++ class-02/warm-up/shredder.js
@@ -3,5 +3,22 @@
 let $$ = {}
 
 $$.map = function(structure,  callback) {
+  // arr === [1,2,3,4]
+  // callback  === (val, idx) => {return val + val}
+  // what happens here????
 
+  const result = [];
+
+  // arr.forEach()
+
+  for (let i = 0; i < arr.length; i++) {
+    // result.push(callback(arr[i], i));
+    result[i] = callback(arr[i], i);
+  }
+
+  return result;
 }
+
+$$.map([1,2,3,4], (val,idx) => {
+
+});
